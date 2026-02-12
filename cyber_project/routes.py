8a9c78ee4e7f30687125e6f8db916063c9f0947d
AlexisPelaez <147647973+AlexisPelaez@users.noreply.github.com>
2026-02-12 11:32:24 -0600

    Add files via upload

---
+++ cyber_project/routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint,render_template, request 
+from .db import get_db
+bp = Blueprint('main',__name__)
+
+# PAGE ROUTING
+
+@bp.route('/')
+def hello_world():
+    return 'Hello World'
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        db.execute("INSERT INTO user (username, password) VALUES (?, ?)",(username,password))
+        db.commit()
+        return f'Hello {username}! Your password is {password}.'
+    return render_template('name.html')
+
+@bp.route('/show')
+def show():
+    db = get_db()
+    users = db.execute("SELECT * FROM user").fetchall()
+    return "<br>".join([f"{u['id']}: {u['username']} - {u['password']}" for u in users])
